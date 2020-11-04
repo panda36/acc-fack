5323e9667000bad82978b72e3c4bb5bcb71ccf19
fakeaccout <67809392+fakeaccout@users.noreply.github.com>
2020-11-04 11:08:17 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const myid = ['Your ID']
+const myid = ['752257355378327743']
 const client = new Discord.Client();
 const client2 = new Discord.Client();
 const client3 = new Discord.Client();
