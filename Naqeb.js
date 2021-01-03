e51e379312ef193d8f3159f7453730b57aab5caf
Naqeb Naqeb <70761216+Naqeb95@users.noreply.github.com>
2021-01-04 00:08:15 +0300

    Naqebup

--- Naqeb.js
+++ Naqeb.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const myid = ['729829613278068776']
+const myid = ['id owner']
 const client = new Discord.Client();
 const client2 = new Discord.Client();
 const client3 = new Discord.Client();
